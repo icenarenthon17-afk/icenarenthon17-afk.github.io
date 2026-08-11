e513ecd38d8a38e1a012bcfdb13b832723ad1df5
icenarenthon17-afk <icenarenthon17@gmail.com>
2026-08-11 07:51:23 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,4 +1,4 @@
 // เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใล้ (Optional)
 document.addEventListener('DOMContentLoaded',function () {
-    console.log('Portfolio ของ ice พร้อมแล้ว!');
+    console.log('Portfolio ของ icenarenthon พร้อมแล้ว!');
 }};
