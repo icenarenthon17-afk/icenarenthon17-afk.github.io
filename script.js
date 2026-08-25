e6551f94b17cd1ce5f82c4d3f6486a1c9139a9ff
icenarenthon17-afk <icenarenthon17@gmail.com>
2026-08-25 09:11:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded',function () {
   document.getElementById('year').innerHTML =new Date ().getFullYear();
     console.log('Portfolio ของ icenarenthon17 พร้อมแล้ว!');
